39eb901f0d0063368101bd87e75f72f99292a754
Cheuk.Yao <cheuk.yao@xinxiangglobal.com>
2020-12-25 15:22:15 +0800

    测试 jodd http的 post 功能

--- com-cheky-springboot-demo/src/test/java/com/cheky/springboot/demo/jodd/HttpTest.java
+++ com-cheky-springboot-demo/src/test/java/com/cheky/springboot/demo/jodd/HttpTest.java
@@ -2,7 +2,9 @@ package com.cheky.springboot.demo.jodd;
 
 import jodd.http.HttpRequest;
 import jodd.http.HttpResponse;
+import jodd.net.HttpMethod;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
 /**
  * @author Cheky
@@ -24,5 +26,14 @@ public class HttpTest {
                 .send();
         var result = response.bodyText();
         System.out.println(result);
+    }
+
+    @Test
+    public void testHttpPost(){
+        String url = "http://localhost:8080/jackson/users/";
+        String jsonString = "{\"lastName\":\"Cheky\",\"email\":\"139@163.com\",\"birthDate\":\"25-12-2020 06:52:51\",\"enabled\":1,\"u_id\":100}";
+        HttpRequest httpRequest = new HttpRequest().method(HttpMethod.POST).set(url).contentTypeJson().bodyText(jsonString,"application/json");
+        var result = httpRequest.send().statusCode();
+        assert result == HttpStatus.CREATED.value();
     }
 }
