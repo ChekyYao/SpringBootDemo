be5ca6a6ee90978c9601e7f276575b2c3e2c5277
Cheky Yao <13925568211@163.com>
2021-01-07 21:21:40 +0800

    使用 Spring Security

---
+++ com-cheky-springboot-demo/src/main/java/com/cheky/springboot/demo/config/WebSecurityConfig.java
@@ -0,0 +1,73 @@
+package com.cheky.springboot.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Autowired
+//    private UserDetailsService userDetailsService;
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 使用自定义登录身份认证组件
+//        auth.authenticationProvider(new JwtAuthenticationProvider(userDetailsService));
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(passwordEncoder().encode("123"))
+                .roles("admin");
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password(passwordEncoder().encode("123"))
+                .roles("normal");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 禁用 csrf, 由于使用的是JWT，我们这里不需要csrf
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                // 跨域预检请求
+                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                // 登录URL
+                .antMatchers("/login").permitAll()
+                // swagger
+                .antMatchers("/swagger**/**").permitAll()
+                .antMatchers("/webjars/**").permitAll()
+                .antMatchers("/v2/**").permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/security")).authenticated();
+                // 其他所有请求需要身份认证
+                //.anyRequest().authenticated();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+}
---
+++ com-cheky-springboot-demo/src/main/java/com/cheky/springboot/demo/controller/SecurityController.java
@@ -0,0 +1,28 @@
+package com.cheky.springboot.demo.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("security")
+public class SecurityController {
+
+    @GetMapping
+    public String hello(){
+        return "hello, spring security!";
+    }
+
+    @GetMapping("admin")
+    @PreAuthorize("hasAnyRole('admin')")
+    public String helloAdmin(){
+        return "hello, the role of admin!";
+    }
+
+    @GetMapping("user")
+    @PreAuthorize("hasAnyRole('user', 'admin')")
+    public String helloUser(){
+        return "hello, the role of user!";
+    }
+}
